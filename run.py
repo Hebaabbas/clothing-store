47ee7847f419a5d4efe804ecec2574aed05643f5
Hebaabbas <heba1996abbas@gmail.com>
2023-04-07 02:13:08 +0000

    Add a function to check the store condition which means if the selling day is done and that all products sold are counted

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+import sys
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
@@ -13,15 +14,37 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('clothing_store')
 
+store_name = input("Hello! This is a clothing store data collector. First, please input the name of the clothing store you'd like to run: ")
+user_age= int(input("And now please input your age: "))
+if user_age < 18:
+    raise SystemExit('The user must be at least 18')
+            
+def store_ready():       
+    while True:
+        store_condition = input("Is the business day over now and you have all the products you've sold counted? please insert yes or no: ")
+        if store_condition.lower() == "yes":
+            print("Great! Now you can document the amount of products sold.")
+            break
+        elif store_condition.lower() == "no":
+            print("Please come back when the business day is over and you have all the products sold counted.")
+            sys.exit()
+        else:
+            print("You did not insert a valid option, please give it another try")
+            continue
+
+       
 def get_sales_data():
     """
     Get sales values input from the user
     """
     while True:
-        print("Welcome to the clothing store sales data collector.")
-        print("Please enter the sales data from the last sales day.")
+
+
+        print("Welcome to " + store_name.capitalize() + "'s sales data collector.")
+        print("Please enter how many products were sold the last business day.")
         print("Data provided are to be 5 different data values, separated by commas.")
-        print("Example: 11,22,33,44,55\n")
+        print("Data provided represents these products in this order: [shirt, jeans, dress, shoe, bag]")
+        print("Data shold be as this Example: 11,22,33,44,55\n")
 
         data_str = input("Enter your data values here: ")
 
@@ -41,7 +64,7 @@ def validate_data(values):
         [int(value) for value in values]
         if len(values) !=5:
             raise ValueError(
-                f"The data provided is {len(values)}, the required data to be entered has to be 6 values"
+                f"The data provided is {len(values)}, the required data to be entered has to be 5 values"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please enter 5 values.\n")
@@ -49,23 +72,21 @@ def validate_data(values):
 
     return True
    
-def update_sales_worksheet(data):
-    """
-    Updates sales values in the worksheet as well as it adds new row with the new added values
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
+
+def calculate_sales_worksheet(data):
+    """to calculate how many products in total we sold at the end of the day"""
+    print("Calculating our sales...\n")
+    total_sales = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales-Worksheet is successfully updated.\n")
-
+"""
 def update_floor_worksheet(data):
-    """
-    Updates floor values in the worksheet as well as it adds new row with the new added values
-    """
+    "Updates floor values in the worksheet as well as it adds new row with the new added values"
     print("Updating floor worksheet...\n")
     floor_worksheet = SHEET.worksheet("floor")
     floor_worksheet.append_row(data)
     print("Floor-Worksheet is successfully updated.\n")
+"""
 
 def update_worksheet(data, worksheet):
     print(f"Updating {worksheet} worksheet...\n")
@@ -93,10 +114,13 @@ def calculate_floor_data(sales_row):
 
 
 def main():
+    store = store_ready()
     data = get_sales_data()
     sales_data=[int(num) for num in data]
     update_worksheet(sales_data, "sales")
     new_floor_data = calculate_floor_data(sales_data)
     update_worksheet(new_floor_data, "floor")
+    storage_data= calculate_storage_data(storage_row)
+    update_worksheet(storage_data, "storage")
 
 main()
